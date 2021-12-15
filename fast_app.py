33c00d486ad4addebfb0a9c002e6d5b86a760f48
Allan Petersen <allanrp03@gmail.com>
2021-12-15 13:24:17 -0600

    app works

--- fast_app.py
+++ fast_app.py
@@ -5,19 +5,20 @@ import requests
 from pandas.core.frame import DataFrame
 import numpy as np
 import pandas as pd
+from fastapi.middleware.cors import CORSMiddleware
 
 nn_model = models.load_model('raw_data/neural_network_v1.h5')
 preproc = joblib.load('raw_data/preprocessor_v1.joblib')
 
 app = FastAPI()
 
-# app.add_middleware(
-#     CORSMiddleware,
-#     allow_origins=["*"],  # Allows all origins
-#     allow_credentials=True,
-#     allow_methods=["*"],  # Allows all methods
-#     allow_headers=["*"],  # Allows all headers
-# )
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 
 @app.get("/")
@@ -25,7 +26,7 @@ def index():
     return {"Buenos Dias": True}
 
 
-@app.get("/predict")
+@app.get("/predict/")
 def predict(value, portion_size, protein, fat, carb, sugar, sodium, calcium,
             kcal, category, index_2019):
 
@@ -41,12 +42,14 @@ def predict(value, portion_size, protein, fat, carb, sugar, sodium, calcium,
         'category': [category],
         '2019': [float(index_2019)]
     }
-    inputs = DataFrame.from_dict(list_of_inputs)
+    inputs = pd.DataFrame.from_dict(list_of_inputs)
 
-    #scaled_inputs = preproc.transform(inputs)
+    scaled_inputs = preproc.transform(inputs)
 
-    #prediction = nn_model.predict(scaled_inputs)
+    prediction = nn_model.predict(scaled_inputs)
 
-    #return prediction
+    prediction_1 = prediction[0][0].tolist()
 
-    return list_of_inputs
+    response = {'Prediction': prediction_1}
+
+    return response
